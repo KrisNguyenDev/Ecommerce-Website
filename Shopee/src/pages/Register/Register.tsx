8b99455e2183cb2ac357a2f4cf694eedcec3758c
Kris Nguyen <kris.nguyen@developertools.com>
2025-01-14 21:31:49 +0700

    register form

--- Shopee/src/pages/Register/Register.tsx
+++ Shopee/src/pages/Register/Register.tsx
@@ -1,3 +1,74 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { LoginBody, LoginDTO } from '@/types/login.type'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Link } from 'react-router-dom'
+
 export default function Register() {
-  return <div>Register</div>
+  const form = useForm<LoginDTO>({
+    resolver: zodResolver(LoginBody),
+  })
+
+  const onSubmit = (values: LoginDTO) => {
+    console.log(values)
+  }
+
+  return (
+    <div className="bg-orange">
+      <div className="max-w-7xl mx-auto p-4 h-full grid grid-cols-1 lg:grid-cols-3 items-center">
+        <Card className="lg:col-start-3">
+          <CardHeader>
+            <CardTitle className="text-2xl">Đăng nhập</CardTitle>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label>Email</Label>
+                      <FormControl>
+                        <Input placeholder="email" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <Label>Mật khẩu</Label>
+                      <FormControl>
+                        <Input placeholder="password" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button className="w-full" type="submit">
+                  Đăng nhập
+                </Button>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="justify-center">
+            <div>
+              Bạn mới biết đến Shopee?{' '}
+              <Link className="text-orange" to="/register">
+                Đăng ký
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      </div>
+    </div>
+  )
 }
