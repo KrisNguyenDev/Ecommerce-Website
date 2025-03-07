9ac3fc726a53b032f2dd3aef2f0042ce21d3564a
Kris Nguyen <kris.nguyen@developertools.com>
2025-03-07 15:49:08 +0700

    add custom color 'myColor2' to Tailwind configuration

--- Shopee/tailwind.config.js
+++ Shopee/tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
       colors: {
         orange: '#ee4d2d',
         myColor: '#ffb6c1',
+        myColor2: '#ffb6c1',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
