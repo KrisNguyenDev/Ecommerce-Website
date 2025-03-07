9c91195e77a231ec90e5401ad9c1c148eaa14b71
Kris Nguyen <kris.nguyen@developertools.com>
2025-03-07 15:52:20 +0700

    remove custom colors 'myColor', 'myColor2', and 'myColor3' from Tailwind configuration

--- Shopee/tailwind.config.js
+++ Shopee/tailwind.config.js
@@ -20,9 +20,6 @@ module.exports = {
     extend: {
       colors: {
         orange: '#ee4d2d',
-        myColor: '#ffb6c1',
-        myColor2: '#ffb6c1',
-        myColor3: '#ffb6c1',
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
