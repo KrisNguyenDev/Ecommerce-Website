a8751c4e27499b05366a23e466edf6d2f2ac53d1
Kris Nguyen <kris.nguyen@developertools.com>
2025-03-13 15:46:51 +0700

    feat: Enhance Header component with search functionality and improved layout

--- Shopee/src/components/Header/Header.tsx
+++ Shopee/src/components/Header/Header.tsx
@@ -2,12 +2,68 @@ import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectVa
 import { useState } from 'react'
 import { LanguageLabel, LanguageType } from '../../enum/language'
 import { Link } from 'react-router-dom'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
 
 export default function Header() {
   const [selectedLanguage, setSelectedLanguage] = useState<LanguageType>(LanguageType.VI)
+  const [searchText, setSearchText] = useState<string>('')
+
   return (
     <div className="pt-2 pb-5 bg-orange text-white">
-      <div className="container max-w-full flex justify-between items-center">
+      <div className="container max-w-full flex justify-end items-center space-x-4">
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+            />
+          </svg>
+          <p>Thông báo</p>
+        </div>
+        <div className="flex items-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+            />
+          </svg>
+          <p>Hỗ trợ</p>
+        </div>
+        <Select
+          value={selectedLanguage}
+          onValueChange={(value: LanguageType) => {
+            setSelectedLanguage(value)
+          }}
+        >
+          <SelectTrigger className="w-[110px] bg-orange border-none focus:ring-0 focus:ring-offset-0">
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value={LanguageType.VI}>{LanguageLabel.get(LanguageType.VI)}</SelectItem>
+              <SelectItem value={LanguageType.EN}>{LanguageLabel.get(LanguageType.EN)}</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      <div className="container max-w-full flex space-x-4 mt-2 items-center">
         <Link to="/">
           <svg viewBox="0 0 192 65" className="h-8 lg:h-11 fill-white">
             <g fillRule="evenodd">
@@ -15,8 +71,13 @@ export default function Header() {
             </g>
           </svg>
         </Link>
-        <div className="flex space-x-4 items-center">
-          <div className="flex items-center">
+        <div className="bg-white p-1 rounded-sm flex w-full">
+          <Input
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            className="text-black border-none"
+          />
+          <Button type="submit" className="border-spacing-2 border-white ">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -28,45 +89,27 @@ export default function Header() {
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
               />
             </svg>
-            <p>Thông báo</p>
-          </div>
-          <div className="flex items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
-              />
-            </svg>
-            <p>Hỗ trợ</p>
-          </div>
-          <Select
-            value={selectedLanguage}
-            onValueChange={(value: LanguageType) => {
-              setSelectedLanguage(value)
-            }}
-          >
-            <SelectTrigger className="w-[110px] bg-orange border-none focus:ring-0 focus:ring-offset-0">
-              <SelectValue />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectItem value={LanguageType.VI}>{LanguageLabel.get(LanguageType.VI)}</SelectItem>
-                <SelectItem value={LanguageType.EN}>{LanguageLabel.get(LanguageType.EN)}</SelectItem>
-              </SelectGroup>
-            </SelectContent>
-          </Select>
+          </Button>
         </div>
+        <Link to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+        </Link>
       </div>
     </div>
   )
