4e787a8c2ab4741a05b6631eaaa093f163a0c378
Kris Nguyen <kris.nguyen@developertools.com>
2025-03-14 15:00:44 +0700

    feat: Implement protected and rejected routes with profile page integration

--- Shopee/src/Route.tsx
+++ Shopee/src/Route.tsx
@@ -1,30 +1,61 @@
-import { useRoutes } from 'react-router-dom'
+import { Navigate, Outlet, useRoutes } from 'react-router-dom'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import ProductList from './pages/ProductList'
 import AuthLayout from './layouts/AuthLayout/AuthLayout'
 import MainLayout from './layouts/MainLayouts'
+import Profile from './pages/Profile'
+
+function ProtectedRoute() {
+  const isAuthenticated = true
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />
+}
+
+function RejectedRoute() {
+  const isAuthenticated = true
+  return isAuthenticated ? <Navigate to="/" /> : <Outlet />
+}
 
 export default function Route() {
   const routeElements = useRoutes([
     {
-      path: '/login',
-      element: (
-        <AuthLayout>
-          <Login />
-        </AuthLayout>
-      ),
+      path: '',
+      element: <RejectedRoute />,
+      children: [
+        {
+          path: '/login',
+          element: (
+            <AuthLayout>
+              <Login />
+            </AuthLayout>
+          ),
+        },
+        {
+          path: '/register',
+          element: (
+            <AuthLayout>
+              <Register />
+            </AuthLayout>
+          ),
+        },
+      ],
     },
     {
-      path: '/register',
-      element: (
-        <AuthLayout>
-          <Register />
-        </AuthLayout>
-      ),
+      path: '',
+      element: <ProtectedRoute />,
+      children: [
+        {
+          path: '/profile',
+          element: (
+            <MainLayout>
+              <Profile />
+            </MainLayout>
+          ),
+        },
+      ],
     },
     {
-      path: '/products',
+      path: '/',
       element: (
         <MainLayout>
           <ProductList />
