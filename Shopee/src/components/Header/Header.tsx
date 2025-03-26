14e34f000f21c4daacc4ca438e85dba8b0334032
Kris Nguyen <kris.nguyen@developertools.com>
2025-03-26 15:45:02 +0700

    feat: Update Avatar component with dynamic image source and enhance Loader styles for better visibility

--- Shopee/src/components/Header/Header.tsx
+++ Shopee/src/components/Header/Header.tsx
@@ -107,7 +107,10 @@ export default function Header() {
         {isAuthenticated ? (
           <div className="flex space-x-2">
             <Avatar className="h-7 w-7">
-              <AvatarImage src="https://github.com/shadcn.png" alt="@user" />
+              <AvatarImage
+                src="https://scontent.fhan12-1.fna.fbcdn.net/v/t1.6435-9/133841700_1470173463187726_7287216184065001525_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_eui2=AeF7V8aTp38y6k1hqYEIHw7EKCDTb3S78RgoINNvdLvxGLJgv5KWh7j87U8GDqwxZyEKr5FMg4IoH-v2ep9xRBiA&_nc_ohc=xhd0E2vE2EAQ7kNvgHrFV-W&_nc_oc=Adlpqq1lq6EIpSchW11rohcZlbIVREtwZg7HMcWwPSDKhhhG7HS6zb8K7EctLLBo5mw&_nc_zt=23&_nc_ht=scontent.fhan12-1.fna&_nc_gid=wLKMvZtm7TuAtA-j2Q84cA&oh=00_AYEMCpqCmL8ApQLv7IDX8beMbY3oi-GgU0ffnrmIOfJZVg&oe=680B3C4F"
+                alt="@user"
+              />
               <AvatarFallback>User</AvatarFallback>
             </Avatar>
             <DropdownMenu>
--- Shopee/src/components/Loader/Loader.tsx
+++ Shopee/src/components/Loader/Loader.tsx
@@ -1,7 +1,7 @@
 export default function Loader() {
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-transparent border-t-gray-400"></div>
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-300 bg-opacity-30">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-transparent border-t-amber-600"></div>
     </div>
   )
 }
