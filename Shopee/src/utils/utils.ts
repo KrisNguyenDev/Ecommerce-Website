b3adf45249b7a688c2526a6d54e907c15d3caf8f
Kris Nguyen <kris.nguyen@developertools.com>
2025-05-21 10:48:34 +0700

    refactor: Rearrange and comment utility functions for better readability

--- Shopee/src/utils/utils.ts
+++ Shopee/src/utils/utils.ts
@@ -1,24 +1,13 @@
 import { ResponseApi } from '@/types/responseApi.type'
 import axios, { AxiosError, HttpStatusCode } from 'axios'
 
+// handle form error
 export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
   return axios.isAxiosError(error)
 }
 
 export function isAxiosUnprocessableEntityError<T>(error: unknown): error is AxiosError<T> {
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
-}
-
-export function formatCurrency(price: string | number) {
-  return new Intl.NumberFormat('vi-VN', {
-    style: 'currency',
-    currency: 'VND',
-  }).format(Number(price))
-}
-
-export function truncateText(text: string, maxLength: number = 10): string {
-  if (text.length <= maxLength) return text
-  return text.slice(0, maxLength) + '...'
 }
 
 export function handleAxiosUnprocessableEntityError<T extends ResponseApi<unknown>, U>(
@@ -34,4 +23,18 @@ export function handleAxiosUnprocessableEntityError<T extends ResponseApi<unknow
       })
     }
   }
+}
+
+// format currency
+export function formatCurrency(price: string | number) {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND',
+  }).format(Number(price))
+}
+
+// truncate text
+export function truncateText(text: string, maxLength: number = 10): string {
+  if (text.length <= maxLength) return text
+  return text.slice(0, maxLength) + '...'
 }
